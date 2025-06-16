8458b7b1dc3bcd7ac8c028f5f3edafec8b8d7fd6
parvashah305 <parvashah305@gmail.com>
2025-06-16 18:09:38 +0530

    cors issue solved

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 const corsOptions = {
-    origin: "http://localhost:5174", 
+    origin: "https://parvashah.vercel.app/", 
     methods: "GET,POST",
     credentials: true
   };
